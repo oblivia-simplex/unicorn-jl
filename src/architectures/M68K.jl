8d38e51e027bd3567bfd0b08425ff0355669e627
lucca <lucca.fraser@gmail.com>
2020-10-15 13:47:12 -0300

    supporting all unicorn architectures now

---
+++ src/architectures/M68K.jl
@@ -0,0 +1,30 @@
+module M68K
+
+# M68K registers
+module Register
+@enum t begin
+    INVALID = 0
+    A0 = 1
+    A1 = 2
+    A2 = 3
+    A3 = 4
+    A4 = 5
+    A5 = 6
+    A6 = 7
+    A7 = 8
+    D0 = 9
+    D1 = 10
+    D2 = 11
+    D3 = 12
+    D4 = 13
+    D5 = 14
+    D6 = 15
+    D7 = 16
+    SR = 17
+    PC = 18
+    ENDING = 19
+end
+
+end
+
+end
---
+++ src/architectures/MIPS.jl
@@ -0,0 +1,201 @@
+module MIPS
+
+# MIPS registers
+module Register
+@enum t begin
+    INVALID = 0
+    # General purpose registers
+    PC = 1
+    R0 = 2
+    R1 = 3
+    R2 = 4
+    R3 = 5
+    R4 = 6
+    R5 = 7
+    R6 = 8
+    R7 = 9
+    R8 = 10
+    R9 = 11
+    R10 = 12
+    R11 = 13
+    R12 = 14
+    R13 = 15
+    R14 = 16
+    R15 = 17
+    R16 = 18
+    R17 = 19
+    R18 = 20
+    R19 = 21
+    R20 = 22
+    R21 = 23
+    R22 = 24
+    R23 = 25
+    R24 = 26
+    R25 = 27
+    R26 = 28
+    R27 = 29
+    R28 = 30
+    R29 = 31
+    R30 = 32
+    R31 = 33
+
+    # DSP registers
+    DSPCCOND = 34
+    DSPCARRY = 35
+    DSPEFI = 36
+    DSPOUTFLAG = 37
+    DSPOUTFLAG16_19 = 38
+    DSPOUTFLAG20 = 39
+    DSPOUTFLAG21 = 40
+    DSPOUTFLAG22 = 41
+    DSPOUTFLAG23 = 42
+    DSPPOS = 43
+    DSPSCOUNT = 44
+
+    # ACC registers
+    AC0 = 45
+    AC1 = 46
+    AC2 = 47
+    AC3 = 48
+
+    # COP registers
+    CC0 = 49
+    CC1 = 50
+    CC2 = 51
+    CC3 = 52
+    CC4 = 53
+    CC5 = 54
+    CC6 = 55
+    CC7 = 56
+
+    # FPU registers
+    F0 = 57
+    F1 = 58
+    F2 = 59
+    F3 = 60
+    F4 = 61
+    F5 = 62
+    F6 = 63
+    F7 = 64
+    F8 = 65
+    F9 = 66
+    F10 = 67
+    F11 = 68
+    F12 = 69
+    F13 = 70
+    F14 = 71
+    F15 = 72
+    F16 = 73
+    F17 = 74
+    F18 = 75
+    F19 = 76
+    F20 = 77
+    F21 = 78
+    F22 = 79
+    F23 = 80
+    F24 = 81
+    F25 = 82
+    F26 = 83
+    F27 = 84
+    F28 = 85
+    F29 = 86
+    F30 = 87
+    F31 = 88
+    FCC0 = 89
+    FCC1 = 90
+    FCC2 = 91
+    FCC3 = 92
+    FCC4 = 93
+    FCC5 = 94
+    FCC6 = 95
+    FCC7 = 96
+
+    # AFPR128
+    W0 = 97
+    W1 = 98
+    W2 = 99
+    W3 = 100
+    W4 = 101
+    W5 = 102
+    W6 = 103
+    W7 = 104
+    W8 = 105
+    W9 = 106
+    W10 = 107
+    W11 = 108
+    W12 = 109
+    W13 = 110
+    W14 = 111
+    W15 = 112
+    W16 = 113
+    W17 = 114
+    W18 = 115
+    W19 = 116
+    W20 = 117
+    W21 = 118
+    W22 = 119
+    W23 = 120
+    W24 = 121
+    W25 = 122
+    W26 = 123
+    W27 = 124
+    W28 = 125
+    W29 = 126
+    W30 = 127
+    W31 = 128
+    HI = 129
+    LO = 130
+    P0 = 131
+    P1 = 132
+    P2 = 133
+    MPL0 = 134
+    MPL1 = 135
+    MPL2 = 136
+    CP0_CONFIG3 = 137
+    CP0_USERLOCAL = 138
+    ENDING = 139
+end
+# Aliases
+ZERO = t(2)
+AT = t(3)
+V0 = t(4)
+V1 = t(5)
+A0 = t(6)
+A1 = t(7)
+A2 = t(8)
+A3 = t(9)
+T0 = t(10)
+T1 = t(11)
+T2 = t(12)
+T3 = t(13)
+T4 = t(14)
+T5 = t(15)
+T6 = t(16)
+T7 = t(17)
+S0 = t(18)
+S1 = t(19)
+S2 = t(20)
+S3 = t(21)
+S4 = t(22)
+S5 = t(23)
+S6 = t(24)
+S7 = t(25)
+T8 = t(26)
+T9 = t(27)
+K0 = t(28)
+K1 = t(29)
+GP = t(30)
+SP = t(31)
+FP = t(32)
+S8 = t(32)
+RA = t(33)
+HI0 = t(45)
+HI1 = t(46)
+HI2 = t(47)
+HI3 = t(48)
+LO0 = t(45)
+LO1 = t(46)
+LO2 = t(47)
+LO3 = t(48)
+end
+end
